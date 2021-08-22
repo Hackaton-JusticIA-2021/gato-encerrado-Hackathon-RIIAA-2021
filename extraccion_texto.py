10f0b88c8c67f48b3fd4242e4bdbb0148544fb45
Rebeca Angulo Rojas <rbbkitanias@gmail.com>
2021-08-22 17:58:35 -0500

    archivos nuevos

---
+++ extraccion_texto.py
@@ -0,0 +1,23 @@
+import pytesseract
+from PIL import Image
+
+
+def extraer_texto_imagen(image_path):
+    im = Image.open(image_path)
+    custom_config = r'--oem 3 --psm 6 --lang spa'
+    texto = pytesseract.image_to_string(im, config=custom_config)
+    return texto
+
+
+def guardar_texto(image_path, files_path):
+    """
+    image_path: directorio donde guarda las imagenes
+    files_path: directorio donde guarda los archivos de texto
+    """
+    texto = extraer_texto_imagen(image_path)
+    filename = image_path.replace('.JPG', '.txt').replace("/", "")
+    os.chdir(dir+"files_path")
+    f = open(filename, "x")  # para que no se duplican
+    f.write(texto)
+    f.close()
+    os.chdir(dir)  # Muestra
