d78620ec7ca8a258a024d154b8f1a743ac3b7e20
Rebeca98 <70819733+Rebeca98@users.noreply.github.com>
2021-08-23 01:27:20 -0500

    Update extraccion_texto.py

--- modules/extraccion_texto.py
+++ modules/extraccion_texto.py
@@ -1,6 +1,6 @@
 import pytesseract
 from PIL import Image
-import os
+
 
 def extraer_texto_imagen(image_path, psm=6):
     im = Image.open(image_path)
@@ -15,14 +15,14 @@ def guardar_texto(image_path, files_path):
     files_path: directorio donde guarda los archivos de texto
     """
     texto = extraer_texto_imagen(image_path)
-    filename = image_path.replace('.JPG', '.txt').replace("/", "")
-    os.chdir(dir+"files_path")
-    f = open(filename, "x")
+    img_name = Path(image_path).name
+    output_filename = img_name.replace('.JPG', '.txt')
+    output_path=str(Path(files_path)/output_filename)
     try:
+        f = open(output_path, "x")
         f.write(texto)
     except:
         print("error en image_path ")
     finally:
         f.close()
-        os.chdir(dir)
-      # Muestra
+    return texto
