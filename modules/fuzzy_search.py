a82b999f3b975c3923e0dafa7c7917261aa738c7
Javier <36058190+Javcm@users.noreply.github.com>
2021-08-23 01:52:44 -0500

    Update fuzzy_search.py

--- modules/fuzzy_search.py
+++ modules/fuzzy_search.py
@@ -17,28 +17,47 @@ def find_matches(name,clase,df):
   return df_salida
 
 
-#Función para encontrar fechas de una ficha 
-def get_dates(archivo,df):
-  
-  if (df["NombreArchivo"]==archivo).sum()==0:
-    print("Registro no encontrado en la base")
+#Función que recibe como argumentos el nombre de un archivo y un data frame con las transcripciones, devuelve un aviso si el nombre del archivo no se encuentra en la base de transcripciones 
+#y devuelve un dataframe con las fechas y número de expediente encontrados en la transcripción. (El formato de salida no corresponde con el reto 2B, este se obtiene más adelante.)
+
+def obtener_df_expedientes_fechas_single(nombre_archivo,df_transcripcion):  
+  if (df_transcripcion["NombreArchivo"]==nombre_archivo).sum()==0:
     return 0
 
   else:
-    text = df[df["NombreArchivo"]==archivo]["Texto"].values[0]
-    df_fechas = pd.DataFrame()
+    text = df_transcripcion[df_transcripcion["NombreArchivo"]==nombre_archivo]["Texto"].values[0]
+    df_fechas_exp = pd.DataFrame()
+    expedientes = []
+    NO_EXPEDIENTE = "No encontrado"
+    j = 0
+    expedientes = []
     for i in text.split():
-        if i.startswith("Exp"):
-            NO_EXPEDIENTE = i
-            text = text.replace(i,'')
-        else:
-          NO_EXPEDIENTE = "No encontrado"
+      j = j+1
+      if i.startswith("Exp"):
+        expedientes.append(i)
+        text = text.replace(i,'')
+        NO_EXPEDIENTE = expedientes[0]
+
     for i in text.split():
       if i.startswith("H-") or i.startswith("L-"):
         text = text.replace(i,'')
     matches = datefinder.find_dates(text)
     for match in matches:
-      df_fechas = df_fechas.append(pd.DataFrame(np.array([archivo, match]).reshape(1,2),
+      df_fechas_exp = df_fechas_exp.append(pd.DataFrame(np.array([nombre_archivo, match]).reshape(1,2),
               columns=["NombreArchivo","fechas"]))
-    df_fechas['Expediente'] = NO_EXPEDIENTE
-  return (df_fechas)
+    df_fechas_exp['Expediente'] = NO_EXPEDIENTE
+  return (df_fechas_exp)
+
+#Función que toma por argumento un archivo de transcripción en el formato que devuelve el reto 2A y devuelve los expedientes y fechas encontrados con el formato del reto 2B. 
+def obtener_df_expedientes_fechas(df_transcripcion):
+  df_fechas_exp = pd.DataFrame()
+  for nombre_archivo in df_transcripcion["NombreArchivo"]:
+    df_fechas_exp = df_fechas_exp.append(obtener_df_expedientes_fechas_single(nombre_archivo,transcripciones))
+
+  df_fechas = df_fechas_exp[["NombreArchivo","fechas"]]
+  df_fechas["clase"] ="Fechas" 
+  df_fechas.columns = ["NombreArchivo","label","clase"]
+  df_expedientes = df_fechas_exp[["NombreArchivo","Expediente"]].groupby(['NombreArchivo','Expediente']).size().reset_index().rename(columns={0:'count'}).drop(["count"],axis =1)
+  df_expedientes["clase"] ="Expediente" 
+  df_expedientes.columns = ["NombreArchivo","label","clase"]
+  return df_expedientes.append(df_fechas)
