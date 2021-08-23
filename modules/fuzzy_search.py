0127221ddb391e3bc42f0be6f2973350213df0d7
Javier <36058190+Javcm@users.noreply.github.com>
2021-08-23 03:23:51 -0500

    Update fuzzy_search.py

--- modules/fuzzy_search.py
+++ modules/fuzzy_search.py
@@ -5,16 +5,31 @@ import datefinder
 import re
 
 
-#Función para encontrar el nombre de las fichas en que se encuentra un nombre
-def find_matches(name,clase,df):
-  df["Index"]=df.index
-  matchs = rapidfuzz.process.extract(name, df["Texto"], scorer=rapidfuzz.fuzz.token_set_ratio,  score_cutoff=80,limit=900)
+#Función que recibe el nombre de alguna entidad a buscar, una clase ('Organización', 'Lugar', 'Servidor Público', 'Enjuiciados') y un dataframe con las transcripciones. 
+#Encuentra los matchs entre la entidad y los textos del 
+
+def find_matches(name,clase,df_transcripciones):
+  df_transcripciones["Index"]=df_transcripciones.index
+  matchs = rapidfuzz.process.extract(name, df_transcripciones["Texto"], scorer=rapidfuzz.fuzz.token_set_ratio,  score_cutoff=85,limit=900)
   df_aux = pd.DataFrame(np.array(matchs).reshape((len(matchs),3)),columns=["label","confianza","Index"])
   df_aux["Index"] = df_aux["Index"].astype(int)
-  df_salida = df_aux.merge(df,  how='inner', on='Index').drop(["Conjunto","Fuente","Texto","MetodoTexto","confianza","Index"],axis = 1)
+  df_salida = df_aux.merge(df_transcripciones,  how='inner', on='Index').drop(["Conjunto","Fuente","Texto","MetodoTexto","confianza","Index"],axis = 1)
   df_salida["label"] = name
   df_salida["clase"] = clase
   return df_salida
+
+#Función que toma como argumentos un dataframe de transcripciones en el formato de salida del reto 2A y una lista de peronas, entidades,etc a buscar 
+#(Nota:Es importante que las listas tengan el mismo formato, es decir nombres de las columnas, con el que se nos proporcionaron al inicio). y devuelve un dataframe con todas las coincidencias encontradas en el formato del reto 2B
+#el parámetro listas debe tener el siguiente formato:
+#listas = [organizaciones,lugares,servidores,enjuiciados]
+
+def obtener_matchs(df_transcripciones,listas):  
+  df_salida = pd.DataFrame([],columns=["label","NombreArchivo","clase"])
+  for lista in listas:
+    for i in range(len(lista)):
+      df_salida = df_salida.append(find_matches(lista.head(i+1)[lista.columns[0]].values[-1],lista.columns[0],df_transcripciones))
+  return df_salida
+
 
 
 #Función que recibe como argumentos el nombre de un archivo y un data frame con las transcripciones, devuelve un aviso si el nombre del archivo no se encuentra en la base de transcripciones 
